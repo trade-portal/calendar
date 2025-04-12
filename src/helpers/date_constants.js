5e6a49e4dae3242521b94c8b5d4b99021221bac8
Nathan Kidd <nathanblenheimkidd@gmail.com>
2025-04-12 13:22:26 +0100

    added helpful date constants

---
+++ src/helpers/date_constants.js
@@ -0,0 +1,50 @@
+export const SUNDAY    = 0
+export const MONDAY    = 1
+export const TUESDAY   = 2
+export const WEDNESDAY = 3
+export const THURSDAY  = 4
+export const FRIDAY    = 5
+export const SATURDAY  = 6
+
+export const JANUARY   = 0
+export const FEBRUARY  = 1
+export const MARCH     = 2
+export const APRIL     = 3
+export const MAY       = 4
+export const JUNE      = 5
+export const JULY      = 6
+export const AUGUST    = 7
+export const SEPTEMBER = 8
+export const OCTOBER   = 9
+export const NOVEMBER  = 10
+export const DECEMBER  = 11
+
+export const THE_FIRST = 1
+
+export const AT_MIDNIGHT = 0
+export const AT_1AM = 1
+export const AT_2AM = 2
+export const AT_3AM = 3
+export const AT_4AM = 4
+export const AT_5AM = 5
+export const AT_6AM = 6
+export const AT_7AM = 7
+export const AT_8AM = 8
+export const AT_9AM = 9
+export const AT_10AM = 10
+export const AT_11AM = 11
+
+export const AT_MIDDAY = 12
+export const AT_1PM    = 13
+export const AT_2PM    = 14
+export const AT_3PM    = 15
+export const AT_4PM    = 16
+export const AT_5PM    = 17
+export const AT_6PM    = 18
+export const AT_7PM    = 19
+export const AT_8PM    = 20
+export const AT_9PM    = 21
+export const AT_10PM   = 22
+export const AT_11PM   = 23
+
+export const START_OF_ALL_TIME = new Date(1970, JANUARY, THE_FIRST, AT_MIDNIGHT)
