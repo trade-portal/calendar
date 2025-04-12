e0da01b5d5d91ed4a4ce31285bcd593c79d5f6d4
Nathan Kidd <nathanblenheimkidd@gmail.com>
2025-04-12 13:24:20 +0100

    testing range grid

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,1 +1,19 @@
 import 'controllers'
+import {
+  RangeGrid,
+  OCTOBER,
+  MONDAY
+} from 'helpers'
+
+const year = 2025
+const month = OCTOBER
+window.start = new Date(year, month, 1)
+window.end   = new Date(year, month, 30)
+
+console.log('start + end')
+console.log(start)
+console.log(end)
+
+window.grid = new RangeGrid(start, end, MONDAY)
+console.log(grid.days)
+console.log('^ grid.days')
